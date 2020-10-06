cf4b1685a6edb440b9c29a82f0833c6bc71a1fae
Mansour <maqsoudi99@gmail.com>
2020-10-05 22:17:26 -0400

    Added multithreading + temp fix to medicine cell radial movement

--- oldA1/main.cpp
+++ oldA1/main.cpp
@@ -1,27 +1,29 @@
 #include <windows.h>
 #include "Dependencies\glew\glew.h"
 #include "Dependencies\freeglut\freeglut.h"
-//#include "Simulation.h" //Simulation file
 #include <iostream>//for printing
-#include <string> // to convert integer to string
-#include <vector> //for storing location of cancer cells during initilization
+#include <vector> //for storing location of medicine cells
 #include <thread> 
 
 using namespace std;
 
 void timer(int);
-void display();
 void keyboard(unsigned char key, int x, int y); //first argument is key press other two arguments are position of mouse
 void generateInitialCancerCells();
-void generateMedicineCells();
 void moveMedicineCells(int i, int j);
 void cellCounter();
-int start;
 void simulation(int start);
+void display();
+void moveExtraMedicine();
+void storeExtraMedicine(int i, int j);
+vector<int> fourCoordinates;
+vector<int> fiveCoordinates;
+vector<int> sixCoordinates;
+vector<int> sevenCoordinates;
 
 //macros for grid
-#define ROWS 60
-#define COLUMNS 60
+#define ROWS 3
+#define COLUMNS 3
 
 //macro for fps
 #define FPS 30
@@ -33,6 +35,8 @@ int cell[ROWS][COLUMNS];
 int totalCells = ROWS * COLUMNS;
 int cancerCellCount = totalCells * 0.40;
 
+
+
 void init() {
 	glClearColor(0.0, 0.0, 0.0, 0.0); //background color
 	glMatrixMode(GL_PROJECTION);
@@ -40,7 +44,6 @@ void init() {
 	//gluOrtho2D(-0.5f, ROWS - 0.5f, -0.5f, COLUMNS - 0.5f);
 }
 
-//this should only be used for changing the values in array not for displaying
 void generateInitialCancerCells()
 {
 	int count = 0;
@@ -54,7 +57,7 @@ void generateInitialCancerCells()
 	}
 }
 
-void setCellColor()
+void setInitialCellColor()
 {
 	for (int i = 0; i < ROWS; i++) {
 		for (int j = 0; j < COLUMNS; j++) {
@@ -86,11 +89,10 @@ void setCellColor()
 
 
 
-
-
 void cellHealthyCheck(int x, int y)
 {
 	int medicineNeighbour = 0;
+
 
 	if (cell[x][y] == 1) //if cell is equal to cancer cell
 	{
@@ -158,42 +160,39 @@ void cellCancerCheck(int x, int y)
 
 }
 
+
+
 void display()
 {
-
 	//clear buffer at the begining of each frame
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	setCellColor(); //set cell color given its current value
+	setInitialCellColor(); //set cell color given its current value
 
-	simulation(start);
+	//Multi-threading
+	std::thread thread1(simulation, ROWS / 3 * 0);
+	std::thread thread2(simulation, ROWS / 3 * 1);
+	std::thread thread3(simulation, ROWS / 3 * 2);
+	
+	//Wait for threads to finish
+	thread1.join();
+	thread2.join();
+	thread3.join();
 
-	//for (int i = start; i < ROWS; i++) {
-	//	for (int j = 0; j < COLUMNS; j++) {
-
-	//		cellCancerCheck(i, j); //check if current cell should become cancerous
-
-	//		cellHealthyCheck(i, j); //check if current cell should become healthy
-
-	//		moveMedicineCells(i, j); //move medicine Cells radially outwards
-
-	//	}
-	//}
-
+	moveExtraMedicine(); //move those specific extra medcine cells to their new cell
+	
 	cellCounter(); //count number of each type of cell, display results
 
 	glutKeyboardFunc(keyboard); //check for user input
 
 	glutSwapBuffers();
-
-
-
 }
+
 
 void simulation(int start)
 {
-	for (int i = start; i < ROWS ; i++) {
-		for (int j = 0; j < COLUMNS ; j++) {
+	for (int i = start; i < start + (ROWS / 3); i++) {
+		for (int j = 0; j < COLUMNS; j++) {
 
 			cellCancerCheck(i, j); //check if current cell should become cancerous
 
@@ -201,10 +200,12 @@ void simulation(int start)
 
 			moveMedicineCells(i, j); //move medicine Cells radially outwards
 
+			//if a cell == 4 || 5 || 6 || 7  ,keep its cell location in a global storage
+			storeExtraMedicine(i, j);
+
 		}
 	}
 }
-
 
 
 int main(int argc, char **argv)
@@ -214,37 +215,11 @@ int main(int argc, char **argv)
 	glutInitWindowSize(900, 900); //original 640 x 480
 	glutCreateWindow("Assignment 1 - Cancer Cell Simulation");
 	init(); //initialize background color and 2D ortho grid
-	generateInitialCancerCells(); //randomly generate initial cancer cells in 2D array
-
-
+	//generateInitialCancerCells(); //randomly generate initial cancer cells in 2D array
 
 	glutDisplayFunc(display);
 
-	//Threading
-	std::thread thread1(simulation, start);
-	cout << "thread 1 started with param " << start << endl;
-	std::thread thread2(simulation, start + (ROWS / 3 * 1));
-	cout << "thread 1 started with param " << start + (ROWS / 3 * 1) << endl;
-	std::thread thread3(simulation, start + (ROWS / 3 * 2));
-	cout << "thread 1 started with param " << start + (ROWS / 3 * 2) << endl;
-
-
-	thread1.join(); //wait for thread one to finish
-	cout << "thread 1 joined" << endl;
-	thread2.join();
-	cout << "thread 2 joined" << endl;
-	thread3.join();
-	cout << "thread 3 joined" << endl;
-
-	//registers a callback after a specified amount of ms have passed (here it only executes once)
-	glutTimerFunc(0, timer, 0); //first argument is when the first frame is displayed, function, int value passed to timer
-
-	//cout << totalCells << endl;
-	//cout << cancerCellCount << endl;
-	//for (auto it = cancerCells.cbegin(); it != cancerCells.cend(); it++)
-	//{
-	//	cout << *it << ' ';
-	//}
+	glutTimerFunc(0, timer, 0); //first argument is when the first frame is displayed, function, int value passed to timer (only executes once)
 
 	//cout << "printing main array" << endl;
 	//for (int i = 0; i < ROWS; i++)
@@ -259,8 +234,45 @@ int main(int argc, char **argv)
 	return 0;
 }
 
+void generateMedicineCells() //this only deels with number and not colors
+{
+	//genrate a random row and column
+	int acceptableRow = ROWS - 2;  //if ROWS = 100, then rand() % acceptableRow go from 0 to 98 (instead of 99)
+	int acceptableColumn = COLUMNS - 2;
+	int row = rand() % acceptableRow + 2; //in the range 1 to 99 (if ROWS == 100)
+	int column = rand() % acceptableColumn + 2;
+
+	if (cell[row][column] == 1)	//if that cell is a cancer cell
+	{
+		cell[row][column] = 0; //change that cancer cell to a healthy cell
+		//change all the cells around it into healthy cells
+		cell[row - 1][column] = 0; //top 
+		cell[row - 1][column + 1] = 0; //top right
+		cell[row][column + 1] = 0; //right
+		cell[row + 1][column + 1] = 0; //bottom right
+		cell[row + 1][column] = 0; //bottom
+		cell[row + 1][column - 1] = 0; //bottom left
+		cell[row][column - 1] = 0; //left
+		cell[row - 1][column - 1] = 0; //top left
+	}
+	else //if its a healthy cell or a medicine cell
+	{
+		//make a square around the cell into medicine cells
+		cell[row - 1][column] = 2; //top 
+		cell[row - 1][column + 1] = 3; //top right
+		cell[row][column + 1] = 4; //right
+		cell[row + 1][column + 1] = 5; //bottom right
+		cell[row + 1][column] = 6; //bottom
+		cell[row + 1][column - 1] = 7; //bottom left
+		cell[row][column - 1] = 8; //left
+		cell[row - 1][column - 1] = 9; //top left
+
+	}
+}
+
 void moveMedicineCells(int i, int j)
 {
+	
 
 	if (cell[i][j] == 2) //if cell is a 2, switch with cell above
 	{
@@ -288,58 +300,59 @@ void moveMedicineCells(int i, int j)
 		}
 	}
 
-	else if (cell[i][j] == 4) //not working properly!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	{
-		if (j + 1 == 0 || i == 0 || j == 0 || i == ROWS - 1 || j == COLUMNS - 1) //if to close to the end of array disappear
-		{
-			cell[i][j] = 0;
-		}
-		else {//move cell right
-			int temp = cell[i][j + 1];
-			cell[i][j] = temp;
-			cell[i][j + 1] = 4;
-		}
+	//else if (cell[i][j] == 4) //not working properly!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	//{
+	//	if (j + 1 == 0 || i == 0 || j == 0 || i == ROWS - 1 || j == COLUMNS - 1) //if to close to the end of array disappear
+	//	{
+	//		cell[i][j] = 0;
+	//	}
+	//	else {//move cell right
+	//		int temp = cell[i][j + 1];
+	//		cell[i][j] = temp;
+	//		cell[i][j + 1] = 4;
+	//	}
+	//
+	//}
+	//else if (cell[i][j] == 5) //not working properly!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	//{
+	//	if (i + 1 == 0 || j + 1 == 0 || i == 0 || j == 0 || i == ROWS - 1 || j == COLUMNS - 1) //if to close to the end of array disappear
+	//	{
+	//		cell[i][j] = 0;
+	//	}
+	//	else {//move cell bottom-right diagonal
+	//		int temp = cell[i + 1][j + 1];
+	//		cell[i][j] = temp;
+	//		cell[i + 1][j + 1] = 5;
+	//	}
+	//	
+	//}
+	//else if (cell[i][j] == 6) //not working properly!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	//{
+	//	if (i + 1 == 0 || i == 0 || j == 0 || i == ROWS - 1 || j == COLUMNS - 1) //if to close to the end of array disappear
+	//	{
+	//		cell[i][j] = 0;
+	//	}
+	//	else {//move cell down
+	//		int temp = cell[i + 1][j];
+	//		cell[i][j] = temp;
+	//		cell[i + 1][j] = 6;
+	//	}
+	//
+	//}
+	//else if (cell[i][j] == 7) //not working properly!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	//{
+	//	if (i + 1 == 0 || j - 1 == 0 || i == 0 || j == 0 || i == ROWS - 1 || j == COLUMNS - 1) //if to close to the end of array disappear
+	//	{
+	//		cell[i][j] = 0;
+	//	}
+	//	else { //move cell bottom-left down
+	//		int temp = cell[i + 1][j - 1];
+	//		cell[i][j] = temp;
+	//		cell[i + 1][j - 1] = 7;
+	//	}
+	//	
+	//}
 
-	}
-	else if (cell[i][j] == 5) //not working properly!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	{
-		if (i + 1 == 0 || j + 1 == 0 || i == 0 || j == 0 || i == ROWS - 1 || j == COLUMNS - 1) //if to close to the end of array disappear
-		{
-			cell[i][j] = 0;
-		}
-		else {//move cell bottom-right diagonal
-			int temp = cell[i + 1][j + 1];
-			cell[i][j] = temp;
-			cell[i + 1][j + 1] = 5;
-		}
-		
-	}
-	else if (cell[i][j] == 6) //not working properly!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	{
-		if (i + 1 == 0 || i == 0 || j == 0 || i == ROWS - 1 || j == COLUMNS - 1) //if to close to the end of array disappear
-		{
-			cell[i][j] = 0;
-		}
-		else {//move cell down
-			int temp = cell[i + 1][j];
-			cell[i][j] = temp;
-			cell[i + 1][j] = 6;
-		}
-
-	}
-	else if (cell[i][j] == 7) //not working properly!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	{
-		if (i + 1 == 0 || j - 1 == 0 || i == 0 || j == 0 || i == ROWS - 1 || j == COLUMNS - 1) //if to close to the end of array disappear
-		{
-			cell[i][j] = 0;
-		}
-		else { //move cell bottom-left down
-			int temp = cell[i + 1][j - 1];
-			cell[i][j] = temp;
-			cell[i + 1][j - 1] = 7;
-		}
-		
-	}
 	else if (cell[i][j] == 8) 
 	{
 		if (i - 1 == 0 || j - 1 == 0 || i == 0 || j == 0 || i == ROWS - 1 || j == COLUMNS - 1) //if to close to the end of array disappear
@@ -368,43 +381,108 @@ void moveMedicineCells(int i, int j)
 
 }
 
-void generateMedicineCells() //this only deels with number and not colors
+void storeExtraMedicine(int i, int j)
 {
-	//genrate a random row and column
-	int acceptableRow = ROWS - 1;  //if ROWS = 100, then rand() % acceptableRow go from 0 to 98 (instead of 99)
-	int acceptableColumn = COLUMNS - 1;
-	int row = rand() % acceptableRow + 1; //in the range 1 to 99 (if ROWS == 100)
-	int column = rand() % acceptableColumn + 1; 
-	
-	if (cell[row][column] == 1)	//if that cell is a cancer cell
+	if (cell[i][j] == 4)
 	{
-		cell[row][column] = 0; //change that cancer cell to a healthy cell
-		//change all the cells around it into healthy cells
-		cell[row - 1][column] = 0; //top 
-		cell[row - 1][column + 1] = 0; //top right
-		cell[row][column + 1] = 0; //right
-		cell[row + 1][column + 1] = 0; //bottom right
-		cell[row + 1][column] = 0; //bottom
-		cell[row + 1][column - 1] = 0; //bottom left
-		cell[row][column - 1] = 0; //left
-		cell[row - 1][column - 1] = 0; //top left
+		fourCoordinates.push_back(j);
+		fourCoordinates.push_back(i);
 	}
-	else //if its a healthy cell or a medicine cell
+	else if (cell[i][j] == 5)
 	{
-		//make a square around the cell into medicine cells
-		cell[row - 1][column] = 2; //top 
-		cell[row - 1][column + 1] = 3; //top right
-		cell[row][column + 1] = 4; //right
-		cell[row + 1][column + 1] = 5; //bottom right
-		cell[row + 1][column] = 6; //bottom
-		cell[row + 1][column - 1] = 7; //bottom left
-		cell[row][column - 1] = 8; //left
-		cell[row - 1][column - 1] = 9; //top left
+		fiveCoordinates.push_back(j);
+		fiveCoordinates.push_back(i);
+	}
+	else if (cell[i][j] == 6)
+	{
+		sixCoordinates.push_back(j);
+		sixCoordinates.push_back(i);
+	}
+	else if (cell[i][j] == 7)
+	{
+		sevenCoordinates.push_back(j);
+		sevenCoordinates.push_back(i);
+
 
 	}
-
-
 }
+
+void moveExtraMedicine()
+{
+	while (!fourCoordinates.empty())
+	{
+		int i = fourCoordinates.back();
+		fourCoordinates.pop_back();
+		int j = fourCoordinates.back();
+		fourCoordinates.pop_back();
+
+		if (j + 1 == 0 || i + 1 == 0 || i == 0 || j == 0 || i == ROWS - 1 || i == ROWS - 2 || j == COLUMNS - 2 || j == COLUMNS - 1 || i == ROWS || j == COLUMNS) //if to close to the end of array disappear
+		{
+			cell[i][j] = 0;
+		}
+		else {//move cell right
+			int temp = cell[i][j + 1];
+			cell[i][j] = temp;
+			cell[i][j + 1] = 4;
+		}
+	}
+
+	while (!fiveCoordinates.empty())
+	{
+		int i = fiveCoordinates.back();
+		fiveCoordinates.pop_back();
+		int j = fiveCoordinates.back();
+		fiveCoordinates.pop_back();
+
+		if (i + 1 == 0 || j + 1 == 0 || i == 0 || j == 0 || i == ROWS - 1 || j == COLUMNS - 1) //if to close to the end of array disappear
+		{
+			cell[i][j] = 0;
+		}
+		else {//move cell bottom-right diagonal
+			int temp = cell[i + 1][j + 1];
+			cell[i][j] = temp;
+			cell[i + 1][j + 1] = 5;
+		}
+	}
+
+	while (!sixCoordinates.empty())
+	{
+		int i = sixCoordinates.back();
+		sixCoordinates.pop_back();
+		int j = sixCoordinates.back();
+		sixCoordinates.pop_back();
+
+		if (i + 1 == 0 || i == 0 || j == 0 || i == ROWS - 1 || j == COLUMNS - 1) //if to close to the end of array disappear
+		{
+			cell[i][j] = 0;
+		}
+		else {//move cell down
+			int temp = cell[i + 1][j];
+			cell[i][j] = temp;
+			cell[i + 1][j] = 6;
+		}
+	}
+
+	while (!sevenCoordinates.empty())
+	{
+		int i = sevenCoordinates.back();
+		sevenCoordinates.pop_back();
+		int j = sevenCoordinates.back();
+		sevenCoordinates.pop_back();
+
+		if (i + 1 == 0 || j - 1 == 0 || i == 0 || j == 0 || i == ROWS - 1 || j == COLUMNS - 1) //if to close to the end of array disappear
+		{
+			cell[i][j] = 0;
+		}
+		else { //move cell bottom-left down
+			int temp = cell[i + 1][j - 1];
+			cell[i][j] = temp;
+			cell[i + 1][j - 1] = 7;
+		}
+
+	}
+}
+
 
 
 void cellCounter()
@@ -436,7 +514,6 @@ void cellCounter()
 
 }
 
-
 void keyboard(unsigned char key, int x, int y) //we dont need to know mouse position
 {
 	if (key == 27) // pressing escape key will end the program
@@ -450,7 +527,6 @@ void keyboard(unsigned char key, int x, int y) //we dont need to know mouse posi
 	}
 }
 
-
 void timer(int)
 {
 	glutPostRedisplay(); //tells opengl to call display function
@@ -458,162 +534,3 @@ void timer(int)
 	//we want 10 fps, 1s = 1000ms ,  1s / 10frames = 10 frames per second 
 	glutTimerFunc(1000 / FPS, timer, 0);
 }
-
-
-//bool checkVector(string str)
-//{
-//	if (count(cancerCells.begin(), cancerCells.end(), str))
-//		return false; //String already exists in the vector
-//	else
-//		return true;
-//}
-
-//void cancerGrowth()
-//{
-//	for (int i = 0; i < COLUMNS; i++)
-//	{
-//		for (int j = 0; j < ROWS; j++)
-//		{
-//			//for now skip first row, first column, last row, last column
-//			int cancerProximityCounter = 0;
-//			if ((cell[i][j] == 0) || (i = 0) || (j = 0) || (i = COLUMNS - 1) || (j = ROWS - 1))
-//			{
-//				continue; //hopefully this brings it back to the top of the inner loop
-//			}
-//			else
-//			{
-//				if (cell[i][j - 1] == 1) //left
-//					cancerProximityCounter++;
-//				if (cell[i - 1][j - 1] == 1) //top left
-//					cancerProximityCounter++;
-//				if (cell[i - 1][j] == 1) //top 
-//					cancerProximityCounter++;
-//				if (cell[i - 1][j + 1] == 1) //top right
-//					cancerProximityCounter++;
-//				if (cell[i][j + 1] == 1) //right
-//					cancerProximityCounter++;
-//				if (cell[i + 1][j + 1] == 1) //bottom right
-//					cancerProximityCounter++;
-//				if (cell[i + 1][j] == 1) //bottom
-//					cancerProximityCounter++;
-//				if (cell[i + 1][j - 1] == 1) //bottom left
-//					cancerProximityCounter++;
-//
-//				if (cancerProximityCounter > 5)
-//				{
-//					cell[i][j] = 1;
-//				}
-//
-//			}
-//
-//
-//
-//
-//		}
-//	}
-//}
-
-//initialized the grid with specific number of columns and rows
-//void initializeScreen()
-//{
-//	//he used glClearColor(0,0,0,1) to set back ground tpo balc 
-//	//might not actually need this function
-//	initializeGrid(COLUMNS, ROWS);
-//}
-
-//called to setup the viewport(resize window) and to setup coordinate system
-
-//string randomCellSelector()
-//{
-//	int v1 = rand() % ROWS;
-//	int v2 = rand() % COLUMNS;
-//	string str1 = to_string(v1);
-//	string str2 = to_string(v2);
-//	string str = str1 + str2;
-//	return str;
-//}
-
-
-
-//void drawCells()
-//{
-//
-//	for (int x = 0; x < COLUMNS; x++)
-//	{
-//		for (int y = 0; y < ROWS; y++)
-//		{
-//			if (cell[x][y] == 0) {
-//				glColor3f(0.2, 0.75, 0.2);//color of the healthy cells
-//				glRectd(x, y, x + 1, y + 1);  //(vertex coordinate, opposite vertex coordinate)
-//			}
-//			if (cell[x][y] == 1) {
-//				glColor3f(0.5, 0.0, 0.0);//color of the healthy cells
-//				glRectd(x, y, x + 1, y + 1);  //(vertex coordinate, opposite vertex coordinate)
-//			}
-//		}
-//	}
-//}
-
-//void reshape_callback(int w, int h)
-//{
-//	//viewport is used when we resize the window
-//	glViewport(0, 0, (GLsizei)w, (GLsizei)h); //top left corner of windows is (0,0) , bottom right corner is (w,h).
-//											  //The arguments take type GLsizei, so we need to cast the integer
-//
-//	//----------Setup the projection for coordinate system----------
-//	//Switch to prjection matrix
-//	glMatrixMode(GL_PROJECTION);
-//	//Make sure no changes at the very beginning
-//	glLoadIdentity();
-//	//We will use orthographic projection because it is a 2D simulation (instead of perspective (3D))
-//	glOrtho(0.0, COLUMNS, 0.0, ROWS, -1.0, 1.0);//leftmost point , rightmost point, bottommost point, topmost point, znear, zfar
-//	//Switch back to the modelview matrix
-//	glMatrixMode(GL_MODELVIEW);
-//
-//}
-
-//void changeColor(GLfloat red, GLfloat green, GLfloat blue) {
-//	r = red;
-//	g = green;
-//	b = blue;
-//}
-
-//Check status of individual cell and apply the game rules.
-//static boolean checkStatus(int x, int y) {
-//	int cancerNeighbour = 0;
-//	bool status = false;
-//
-//	if (cell[x][y] == 2 || cell[x][y] == 3 || cell[x][y] == 4 || cell[x][y] == 5 || cell[x][y] == 6 || cell[x][y] == 7 || cell[x][y] == 8 || cell[x][y] == 9) 
-//	{
-//		status = false;
-//	}
-//	else 
-//	{
-//		for (int i = (x - 1); i < (x + 2); i++)
-//		{
-//			if (cell[i][y - 1] == true)
-//			{
-//				cancerNeighbour++;
-//			}
-//			if (cell[i][y + 1] == true)
-//			{
-//				cancerNeighbour++;
-//			}
-//		}
-//		if (cell[x - 1][y] == true)
-//		{
-//			cancerNeighbour++;
-//		}
-//		if (cell[x + 1][y] == true)
-//		{
-//			cancerNeighbour++;
-//		}
-//
-//		if (cancerNeighbour > 5)
-//		{
-//			status = true;
-//		}
-//	}
-//
-//	return status;
-//}
